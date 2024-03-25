a8d3aef849315dd0d2e2f4ea283343b70b34eb05
Montserrat Ramirez <ramirezpinomontserrat@gmail.com>
2024-03-25 19:16:10 -0300

    6.ls Arreglado

--- 6.ls.js
+++ 6.ls.js
@@ -1,10 +1,10 @@
 const fs = require('node:fs')
 //en los call back siempre el error es el primer parametro
 //asi obliga a pensar que el error esta esperando
-fs.readdir('-', (err,files) => {
+fs.readdir('.', (err,files) => {
     if (err) {
         console.error('Error al leer el directorio: ', err)
-        return
+        return;
     }
     files.forEach(file => {
         console.log(file)
